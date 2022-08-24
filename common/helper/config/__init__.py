75b43b223262731c1b67bcbb6161214520274702
Luu Minh Tuan <tuanlm@ssi.com.vn>
2022-08-24 11:20:07 +0700

    added performance_backtest

---
+++ common/helper/config/__init__.py
@@ -0,0 +1,3 @@
+from .utils import RunMode, RunDate, Domain, today, yesterday, date_range, month_range, parse_bool, timestamp_to_datetime
+from .config import Config
+config = Config()
---
+++ performance_backtest/main.py
@@ -0,0 +1,3 @@
+from common.helper.config import config
+
+run_date = config.run_date
